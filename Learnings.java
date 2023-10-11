6d7230286ab88d8759fe424275f0de2e61553773
Sam <112262318+someonewearingglasses@users.noreply.github.com>
2023-10-11 14:51:28 +0800

    Add files via upload

---
+++ Learnings.java
@@ -0,0 +1,12 @@
+public class Learnings {
+    public static void main(String[] args) {
+
+        //f-string format
+        String name = "James";
+        int age = 19;
+        System.out.printf("I am %s, %d years old ", name, age);
+
+        
+    }
+    
+}
