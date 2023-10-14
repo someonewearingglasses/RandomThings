69c6f8f53648d220198a041911d094619640b996
Sam <112262318+someonewearingglasses@users.noreply.github.com>
2023-10-14 23:39:35 +0800

    Update Learnings.java

--- Learnings.java
+++ Learnings.java
@@ -6,7 +6,17 @@ public class Learnings {
         int age = 19;
         System.out.printf("I am %s, %d years old ", name, age);
 
-        
+        //Converting data types
+        int i = 5;
+        char c = 5;
+        String s = 5;
+            // any(specific object) to string
+        String is = Integer.toString(i);
+        String cs = Character.toString(c);
+            // String to int
+        int si = Integer.parseInt(i);
+            // char to int
+        int ci1 = Integer.parseInt(Character.toString(c));
+        int ci2 = c - '0';
     }
-    
 }
